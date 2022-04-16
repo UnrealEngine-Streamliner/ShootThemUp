ac5bf52b11b23a51f809e93caa1ed8032df62b57
Tiger <thisisstreamliner@gmail.com>
2022-04-16 19:18:06 +0300

    Add Movement.

--- Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
+++ Source/ShootThemUp/Private/Player/STUBaseCharacter.cpp
@@ -33,5 +33,19 @@ void ASTUBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("MoveForward", this, &ASTUBaseCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ASTUBaseCharacter::MoveRight); 
 }
+
+void ASTUBaseCharacter::MoveForward(float Amount)
+{
+	AddMovementInput(GetActorForwardVector(), Amount);
+}
+
+void ASTUBaseCharacter::MoveRight(float Amount)
+{
+	AddMovementInput(GetActorRightVector(), Amount);
+}
+
+
 
